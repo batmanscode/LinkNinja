13e8415d20a8dcd76a0dd9c19e9dae8be9a19f1d
batmanscode <29989939+batmanscode@users.noreply.github.com>
2022-12-16 07:02:15 +0530

    Setup nbdev (#1)
    * Set up nbdev and other small changes
    
    1. Set up nbdev
    
    2. Updated 00_link_ninja.ipynb:
    - use ULID instead of unix timestamp
    - webhooks only run if it's not already set
    
    3. Added a deta.space Spacefile
    
    * remove pinning deta version
    
    * pin python to 3.9
    
    `deta` doesn't work on anything above this and so was getting a module not found error
    
    updated this based on: https://forums.fast.ai/t/python-version-error-while-running-ci-operations-in-github/98482/4
    
    * add deta version

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+pip install notebook
+pip install nbdev
+nbdev_install_quarto
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+jupyter nbextension enable collapsible_headings/main
+jupyter nbextension enable toc2/main
+pip install jupyter_nbextensions_configurator
+nbdev_install_hooks
